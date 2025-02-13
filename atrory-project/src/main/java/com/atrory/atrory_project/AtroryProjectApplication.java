46910ee5dbe0f56c85d203fb53b2489498b2399a
AkyParfait <agathariell@gmail.com>
2025-02-13 16:42:26 -0300

    Primeiro commit

---
+++ atrory-project/src/main/java/com/atrory/atrory_project/AtroryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.atrory.atrory_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtroryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtroryProjectApplication.class, args);
+	}
+
+}
---
+++ atrory-project/src/test/java/com/atrory/atrory_project/AtroryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atrory.atrory_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtroryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
