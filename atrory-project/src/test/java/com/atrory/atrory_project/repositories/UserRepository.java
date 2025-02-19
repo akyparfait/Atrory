f93b4b9497871986728b323573f5751d86d8c928
Akyparfait <agathariell@gmail.com>
2025-02-19 16:21:49 -0300

    repositório para interagir com o banco de dados

---
+++ atrory-project/src/test/java/com/atrory/atrory_project/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.atrory.atrory_project.repositories;
+
+import java.util.Optional;  
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.atrory.atrory_project.models.User;
+
+@Repository  
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
